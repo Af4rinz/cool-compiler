666994662d45792cb35425ae18bf031d0df4c989
Afarin Zamanian <afzado@gmail.com>
2021-03-29 21:02:18 +0430

    get input path from user

--- compiler/CoolCompiler.java
+++ compiler/CoolCompiler.java
@@ -1,6 +1,9 @@
 package compiler;
 
 import generator.*;
+
+import java.util.Scanner;
+
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
@@ -12,7 +15,13 @@ import org.antlr.v4.runtime.tree.ParseTreeWalker;
 public class CoolCompiler {
 
     public static void main(String[] args) throws Exception {
-        String input = "../samples/sample2.txt";
+        String input;
+        // input = "../samples/Sample1.txt";
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter path to code snippet: ");
+        input = sc.nextLine();        
+        sc.close();
+
         CharStream stream = CharStreams.fromFileName(input);
         CoolLexer lexer = new CoolLexer(stream);
         TokenStream tokens = new CommonTokenStream(lexer);
@@ -20,8 +29,10 @@ public class CoolCompiler {
         parser.setBuildParseTree(true);
         ParseTree tree = parser.program();
         ParseTreeWalker walker = new ParseTreeWalker();
+
         String output = input.split(".txt")[0] + "_out.txt";
         CoolListener  listener = new CoolPrinter(output);
+        
         walker.walk(listener, tree);
     }
 }
